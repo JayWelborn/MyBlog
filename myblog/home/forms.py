06ff49e42a35dc05dbe02545783439190c80cffd
JayWelborn <jesse.welborn@gmail.com>
2017-01-27 07:18:49 +0100

    27-Jan-2017

--- myblog/home/forms.py
+++ myblog/home/forms.py
@@ -27,8 +27,11 @@ class ContactForm(forms.Form):
             # instantiates EmailMessage class with data from form
             contact_email = EmailMessage(subject=self.cleaned_data['subject'],
                                          to=['jesse.welborn@gmail.com'],
-                                         body='Sender Name: {} \n\n{}'.format(self.cleaned_data['name'],
-                                                                              self.cleaned_data['message']))
+                                         body='Sender Name: {} \nSender Email: {}\n\n {}'.format(
+                                             self.cleaned_data['name'],
+                                             self.cleaned_data['email'],
+                                             self.cleaned_data['message']
+                                         ))
 
             # adds cc line if applicable
             cc_myself = self.cleaned_data['cc_myself']

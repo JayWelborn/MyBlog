b9a2c0cc33fa3523ea4fa88d36a9e75c1553ca0b
Jay Welborn <jesse.welborn@gmail.com>
2017-05-08 10:17:49 +0200

    updated prism settings for bash scripts

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -172,5 +172,18 @@ TINYMCE_DEFAULT_CONFIG = {
         {'start': '* ', 'cmd': 'InsertUnorderedList'},
         {'start': '- ', 'cmd': 'InsertUnorderedList'}
     ],
+    'codesample_languages': [
+        {'text': 'HTML/XML', 'value': 'markup'},
+        {'text': 'JavaScript', 'value': 'javascript'},
+        {'text': 'CSS', 'value': 'css'},
+        {'text': 'PHP', 'value': 'php'},
+        {'text': 'Ruby', 'value': 'ruby'},
+        {'text': 'Python', 'value': 'python'},
+        {'text': 'Java', 'value': 'java'},
+        {'text': 'C', 'value': 'c'},
+        {'text': 'C#', 'value': 'csharp'},
+        {'text': 'C++', 'value': 'cpp'},
+        {'text': 'Bash', 'value': 'bash'}
+    ],
 }
 TINYMCE_SPELLCHECKER = False
